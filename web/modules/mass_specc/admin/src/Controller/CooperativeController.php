ad7819cf12fac0c1da295c5866a1720424ee1577
Anthony Yap <anthony.yap@orangeandbronze.com>
2025-09-10 01:49:56 +0000

    feat:add base theme and content types

--- web/modules/mass_specc/admin/src/Controller/CooperativeController.php
+++ web/modules/mass_specc/admin/src/Controller/CooperativeController.php
@@ -3,24 +3,10 @@
 namespace Drupal\admin\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Form\FormBuilderInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class CooperativeController extends ControllerBase {
 
-    protected $formBuilder;
-    public function __construct(FormBuilderInterface $form_builder) {
-        $this->formBuilder = $form_builder;
+    public function addCooperative(): array {
+        return $this->formBuilder()->getForm(\Drupal\admin\Form\CooperativeCreateForm::class);
     }
-
-    public static function create(ContainerInterface $container) {
-        return new static(
-            $container->get('form_builder')
-        );
-    }
-
-    public function createForm() {
-        return $this->formBuilder->getForm('\Drupal\admin\Form\CooperativeCreateForm');
-    }
-
 }
---
+++ web/themes/custom/mass_specc_bootstrap_sass/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.mass_specc_bootstrap_sass = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
